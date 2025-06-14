823d0f2ab489aa108c6b9c1d26fea42e72481153
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-06-14 19:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+// validation.js
+
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
+
